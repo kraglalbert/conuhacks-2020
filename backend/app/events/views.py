83f89a6066b94550a8009d965fa8f3ed0974b671
kraglalbert <albert.kragl@mail.mcgill.ca>
2020-01-25 18:09:47 -0500

    Add routes to add and remove attendees from an event

--- backend/app/events/views.py
+++ backend/app/events/views.py
@@ -26,11 +26,10 @@ def get_events_by_filter():
 @events.route("", methods=["POST"])
 def create_event():
     data = request.get_json(force=True)
-    event_name = data.get("name")
+    event_name = data.get("event_name")
     description = data.get("description")
     location = data.get("location")
     category = data.get("category")
-    tags = data.get("tags")
     datetime_str = data.get("date_time")
     host_email = data.get("host_email")
 
@@ -40,7 +39,6 @@ def create_event():
         or location == ""
         or host_email == ""
         or category == ""
-        or tags is None
     ):
         abort(400, "Cannot have empty fields for new event")
 
@@ -48,15 +46,58 @@ def create_event():
     if host is None:
         abort(400, "Event host does not exist")
 
-    datetime_obj = datetime.strptime(datetime_str, "%D-%M-%Y %I:%M%p")
+    # format: 12-12-2019 1:30PM
+    datetime_obj = datetime.strptime(datetime_str, "%d-%m-%Y %I:%M%p")
     new_event = Event(
         name=event_name,
         date_time=datetime_obj,
-        category=category,
-        tags=tags,
+        category=EventCategory(category),
         host=host.id,
     )
     db.session.add(new_event)
     db.session.commit()
 
     return jsonify(new_event.serialize)
+
+
+# add a new attendee to an event
+@events.route("/<int:event_id>/add-attendee", methods=["PUT"])
+def add_attendee_to_event(event_id):
+    data = request.get_json(force=True)
+    attendee_email = data.get("attendee_email")
+
+    if attendee_email is None or attendee_email == "":
+        abort(400, "Attendee email cannot be empty")
+
+    attendee = User.query.filter_by(email=attendee_email).first()
+    event = Event.query.filter_by(id=event_id).first()
+
+    event.attendees.append(attendee)
+    db.session.add(event)
+    db.session.commit()
+
+    return jsonify(event.serialize)
+
+
+# remove an attendee from an event
+@events.route("/<int:event_id>/remove-attendee", methods=["PUT"])
+def remove_attendee_from_event(event_id):
+    data = request.get_json(force=True)
+    attendee_email = data.get("attendee_email")
+
+    if attendee_email is None or attendee_email == "":
+        abort(400, "Attendee email cannot be empty")
+
+    attendee = User.query.filter_by(email=attendee_email).first()
+    event = Event.query.filter_by(id=event_id).first()
+
+    cur_attendees = event.attendees
+
+    # filter out the attendee that should be removed
+    filtered_attendees = list(filter(lambda a: a.id != attendee.id, cur_attendees))
+
+    event.attendees = filtered_attendees
+    db.session.add(event)
+    db.session.commit()
+
+    return jsonify(event.serialize)
