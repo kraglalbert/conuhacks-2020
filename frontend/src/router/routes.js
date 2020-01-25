f1b99b49bd44dd760033974d87e279fcb16b0d86
PaulHooley <paul.hooley@mail.mcgill.ca>
2020-01-25 17:06:44 -0500

    Fix router issues

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,32 +1,25 @@
+
 const routes = [
   {
-    path: "",
-    redirect: "/home"
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Dashboard.vue') }
+    ]
   },
-  // {
-  //   path: "/home",
-  //   component: () => import("layouts/MainLayout.vue"),
-  //   meta: { requiresAuth: true },
-  //   children: [{ path: "", component: () => import("pages/HomePage.vue") }]
-  // },
   {
     path: "/login",
     component: () => import("pages/LoginPage.vue"),
     children: []
-  },
-  {
-    path: "/",
-    component: () => import("layouts/MyLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Dashboard.vue") }]
   }
-];
+]
 
 // Always leave this as last one
-if (process.env.MODE !== "ssr") {
+if (process.env.MODE !== 'ssr') {
   routes.push({
-    path: "*",
-    component: () => import("pages/Error404.vue")
-  });
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
 }
 
-export default routes;
+export default routes
