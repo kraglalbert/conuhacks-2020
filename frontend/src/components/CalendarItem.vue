ad2bc1b96a4c9a2edfab1a0b8d3d6df311455546
PaulHooley <paul.hooley@mail.mcgill.ca>
2020-01-26 00:19:29 -0500

    Calendar working!

---
+++ frontend/src/components/CalendarItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="text-h4 q-mb">{{event.name}}</div>
+        <p>{{event.description}}</p>
+    </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: 'CalendarItemList',
+  props: {
+    event: Object
+  }
+}
+</script>
