ed422fcb4f3afe02760c29c278ab956dea55e58f
PaulHooley <paul.hooley@mail.mcgill.ca>
2020-01-25 19:12:20 -0500

    activity compoenent

--- frontend/src/pages/Activities.vue
+++ frontend/src/pages/Activities.vue
@@ -1,3 +1,13 @@
 <template>
-    
+    <activity activityName="Hello World" date="1/1/2020"/>
 </template>
+
+<script>
+import activity from '../components/Activity.vue'
+
+export default {
+    components: {
+        activity
+    }    
+}
+</script>
