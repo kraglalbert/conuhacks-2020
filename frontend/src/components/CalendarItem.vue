df248566b1952d1cb21c52f2231443675ebd2b5c
PaulHooley <paul.hooley@mail.mcgill.ca>
2020-01-26 00:49:32 -0500

    Improve calendar

--- frontend/src/components/CalendarItem.vue
+++ frontend/src/components/CalendarItem.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
         <div class="text-h4 q-mb " >{{event.name}}</div>
-        <p class="bg-">{{event.description}}</p>
+        <p>{{event.date_time}}</p>
+        <q-badge class="primary">{{event.category}}</q-badge>
+            <p>{{event.description}}</p>
+        <q-separator />
     </div>
 </template>
 
